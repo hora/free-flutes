b8a397542ac6f96c9f481f94063107a8a095ac15
zmrzl <zmrzl@protonmail.com>
2021-07-07 18:20:09 -0400

    adds hard-coded team info box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team/team.component';
+import { TeamInfoComponent } from './team-info/team-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent,
+    TeamInfoComponent
   ],
   imports: [
     BrowserModule
